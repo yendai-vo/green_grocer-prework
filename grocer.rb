ae1a09c9329531daf2dd066a8df4f7811b07b633
Cindy Vo <yendai.vo@hotmail.com>
2018-07-15 04:11:26 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -2,9 +2,15 @@ def consolidate_cart(cart)
   # code here
   consolidatedCart = {}
 
-  cart.each do |vegetable, details|
-    consolidatedCart[vegetable] = details
-    # consolidatedCart[vegetable].merge({:count => 1})
+  cart.each do |vegetableHash|
+    currentVegetable = vegetableHash.keys[0]
+    currentVegetableDetails = vegetableHash.values[0]
+    if consolidatedCart[currentVegetable]
+      consolidatedCart[currentVegetable][:count] +=1
+    else 
+      consolidatedCart[currentVegetable] = currentVegetableDetails
+      consolidate_cart[currentVegetable][:count] = 1
+    end
   end
   puts consolidatedCart
   return consolidatedCart
