9961cdabbbb1d6bda9371516cce224a5cd2b2eaa
Cindy Vo <yendai.vo@hotmail.com>
2018-07-15 05:26:03 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -44,7 +44,7 @@ end
 def apply_clearance(cart)
   # code here
   # puts cart
-  cart.transform_values do |values|
+  something = cart.transform_values do |values|
     # return values
     return {:price=> 1, :clearance=> 1, :count=>1}
     # {
@@ -53,8 +53,8 @@ def apply_clearance(cart)
     #   :count => values[:count]
     # }
   end
-  puts cart
-  return cart
+  puts something
+  return something
 end
 
 def checkout(cart, coupons)
