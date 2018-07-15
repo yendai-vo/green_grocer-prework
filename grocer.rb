47b4589b9e7636f8dd473177ff37689120386e1f
Cindy Vo <yendai.vo@hotmail.com>
2018-07-15 05:08:04 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -43,7 +43,7 @@ def apply_coupons(cart, coupons)
       end
     end
   else
-    cartWithCoupons[vegetable] = details
+    # cartWithCoupons[vegetable] = details
   end
   puts cartWithCoupons
   return cartWithCoupons
